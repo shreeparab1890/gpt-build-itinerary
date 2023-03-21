afbdb383748e665ff11e980616da783d985ae1ad
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2023-03-21 14:42:03 +0530

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,18 @@
+export default function Head() {
+  return (
+    <>
+      <title>HinterScaes | Build your own Itenerary</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Build your custom travel itenerary powered by GPT3"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7265051125086713"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { AnalyticsWrapper } from "./components/analytics";
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body style={{ backgroundColor: "white" }}>
+        <div className="wrapper">
+          {children}
+          <AnalyticsWrapper />
+        </div>
+        <footer className="footer" style={{ backgroundColor: "#1A2B48" }}>
+          <p style={{ color: "white" }}>Hinterscapes</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
