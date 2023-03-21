cb89d28e39f6802723d4dc0e7af6575e927a82ea
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2023-03-21 21:50:29 +0530

    Update get-itinerary.ts

--- pages/api/get-itinerary.ts
+++ pages/api/get-itinerary.ts
@@ -48,7 +48,7 @@ export default async function handler(
         model: 'text-davinci-003',
         prompt: basePrompt,
         temperature: 0,
-        max_tokens: 800
+        max_tokens: 550
       })
     })
     const itinerary = await response.json()
