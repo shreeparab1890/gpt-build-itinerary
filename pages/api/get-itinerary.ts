1236890f639c90a17a6f860a86c3cb9ae121e278
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2023-03-22 17:53:06 +0530

    Update get-itinerary.ts

--- pages/api/get-itinerary.ts
+++ pages/api/get-itinerary.ts
@@ -45,10 +45,10 @@ export default async function handler(
       method: 'POST',
       headers,
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
+        model: 'text-davinci-003',
         prompt: basePrompt,
         temperature: 0,
-        max_tokens: 550
+        max_tokens: 2500
       })
     })
     const itinerary = await response.json()
